ae37494c269af9d0782a63d5989455622a5b707c
Tomasz <tomaszbochenek04@gmail.com>
2025-10-22 13:57:15 +0200

    Zmiany w Java

--- app/src/main/java/com/example/sprawdzian/MainActivity.java
+++ app/src/main/java/com/example/sprawdzian/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.sprawdzian;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView test=new TextView(getApplicationContext());
     }
 }
